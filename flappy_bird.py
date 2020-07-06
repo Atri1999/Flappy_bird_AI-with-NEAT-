3944a60d88683ba62bc22aba9767d6e489d06c0e
Atri Chakraborty <chakrabortyatri1999@gmail.com>
2020-07-06 08:54:59 +0530

    This configure the neat into the program. It adds a neat configure file and the configuation in a program

--- flappy_bird.py
+++ flappy_bird.py
@@ -217,7 +217,14 @@ def main():
 
 main()    
 
+def run(config_path):
+    config=neat.config.Config(neat.DefaultGenome,neat.DefaultReproduction,neat.DefaultSpeciesSet,neat.DefaultStagnation,config_path)
+    
 
+if __name__="__main__":
+    local_dir=os.path.dirname(__file__)
+    config_path=os.path.join(local_dir,"config_neat.txt")
+    run(config_path)
 
 
 
